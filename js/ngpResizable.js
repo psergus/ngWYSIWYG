726199da608658cfe02c1d05f806d8fcf1a6d115
brunokrebs <krebs.bruno@gmail.com>
2016-01-29 19:54:51 -0200

    mousemove has to be listened by editor's iframe as well

--- js/ngpResizable.js
+++ js/ngpResizable.js
@@ -16,24 +16,27 @@
 				var iframes = doc.querySelectorAll('iframe');
 				for (var i = 0; i < iframes.length; i++) {
 					iframes[i].contentWindow.document.addEventListener('mouseup', stopResizing);
+					iframes[i].contentWindow.document.addEventListener('mousemove', resize);
 				}
 
 				function resize(event) {
 					event.preventDefault();
 
 					// Function to manage resize down event
-					var margin = 50;
-					var uppest = element.offsetTop + margin;
-					var height = margin;
-					if (event.pageY > uppest) {
-						height = event.pageY - (element.getBoundingClientRect().top + event.view.pageYOffset);
+					var cursorVerticalPosition = event.pageY;
+					if (event.view != doc.defaultView) {
+						// we are hover our iframe
+						cursorVerticalPosition = event.pageY +
+							event.view.frameElement.getBoundingClientRect().top +
+							doc.defaultView.pageYOffset;
 					}
+					var height = cursorVerticalPosition - (element.getBoundingClientRect().top + doc.defaultView.pageYOffset);
 
 					var currentHeight = element.style.height.replace('px', '');
 					if (currentHeight && currentHeight < height &&
 						window.innerHeight - event.clientY <= 45) {
 						// scrolling to improve resize usability
-						event.view.scrollBy(0, height - currentHeight);
+						doc.defaultView.scrollBy(0, height - currentHeight);
 					}
 
 					element.style.height = height + 'px';
@@ -46,6 +49,7 @@
 					var iframes = doc.querySelectorAll('iframe');
 					for (var i = 0; i < iframes.length; i++) {
 						iframes[i].contentWindow.document.removeEventListener('mouseup', stopResizing);
+						iframes[i].contentWindow.document.removeEventListener('mousemove', resize);
 					}
 				}
 			});
