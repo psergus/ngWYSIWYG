bc303e26c060f92d24f802591a05c28e175392dd
brunokrebs <krebs.bruno@gmail.com>
2016-01-29 18:58:01 -0200

    Some cleaning on resize directive - no more jquery/jqlite functions used

--- js/ngpResizable.js
+++ js/ngpResizable.js
@@ -5,39 +5,39 @@
 			var doc = $document[0];
 			var element = $element[0];
 
-			var resizeDown = function($event) {
-				// Function to manage resize down event
-				var margin = 50;
-				var uppest = element.offsetTop + margin;
-				var height = margin;
-				if ($event.pageY > uppest) {
-					height = $event.pageY - (element.getBoundingClientRect().top + $event.view.pageYOffset);
+			var resizeButton = doc.createElement('span');
+			resizeButton.className = 'resizer';
+			element.appendChild(resizeButton);
+
+			resizeButton.addEventListener('mousedown', function() {
+				doc.addEventListener('mousemove', resize);
+				doc.addEventListener('mouseup', stopResizing);
+				doc.defaultView.addEventListener('blur', stopResizing);
+
+				function resize(event) {
+					event.preventDefault();
+					// Function to manage resize down event
+					var margin = 50;
+					var uppest = element.offsetTop + margin;
+					var height = margin;
+					if (event.pageY > uppest) {
+						height = event.pageY - (element.getBoundingClientRect().top + event.view.pageYOffset);
+					}
+
+					var currentHeight = element.style.height.replace('px', '');
+					if (currentHeight && currentHeight < height &&
+						window.innerHeight - event.clientY <= 45) {
+						// scrolling to improve resize usability
+						event.view.scrollBy(0, height - currentHeight);
+					}
+
+					element.style.height = height + 'px';
 				}
 
-				var currentHeight = element.style.height.replace('px', '');
-				if (currentHeight && currentHeight < height &&
-					window.innerHeight - $event.clientY <= 45) {
-					// scrolling to improve resize usability
-					$event.view.scrollBy(0, height - currentHeight);
-				}
-
-				element.style.height = height + 'px';
-			};
-
-			var newElement = angular.element('<span class="resizer"></span>');
-			$element.append(newElement);
-			newElement[0].addEventListener('mousedown', function($event) {
-				doc.addEventListener('mousemove', mousemove);
-				doc.addEventListener('mouseup', mouseup);
-
-				function mousemove($event) {
-					$event.preventDefault();
-					resizeDown($event);
-				}
-
-				function mouseup() {
-					doc.removeEventListener('mousemove', mousemove);
-					doc.removeEventListener('mouseup', mouseup);
+				function stopResizing() {
+					doc.removeEventListener('mousemove', resize);
+					doc.removeEventListener('mouseup', stopResizing);
+					doc.defaultView.removeEventListener('blur', stopResizing);
 				}
 			});
 		};
